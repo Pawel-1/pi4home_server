f7e6a2f3b02106228c2717e48675bd3f465b24c6
Pawel <pawel.grzyb1985@gmail.com>
2019-01-10 22:28:42 +0100

    added dummy implementation of RabbitMq for testing

--- src/main/java/com/pi4home/server/controller/HelloWorld.java
+++ src/main/java/com/pi4home/server/controller/HelloWorld.java
@@ -1,14 +1,28 @@
 package com.pi4home.server.controller;
 
+import com.pi4home.server.QueueProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld
 {
+    @Autowired
+    QueueProducer queueProducer;
+
     @RequestMapping("/hello")
     public String initDb()
     {
+        try
+        {
+            queueProducer.produce("hello from rabbit");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
         return "Hello World!";
     }
 
