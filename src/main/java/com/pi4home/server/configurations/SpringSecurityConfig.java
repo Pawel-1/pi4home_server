549e17c2bbe18c0241adf38286512bf2f0f0dd13
Pawel <pawel.grzyb1985@gmail.com>
2019-03-29 21:59:53 +0100

    extended autorization

---
+++ src/main/java/com/pi4home/server/configurations/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.pi4home.server.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser(("${spring.security.user.name}"))
+                .password("${spring.security.user.password}")
+                .roles("USER");
+    }
+}
