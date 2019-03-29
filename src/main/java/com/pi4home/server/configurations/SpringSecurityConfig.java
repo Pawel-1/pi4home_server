1898eae52fffc6c0132cc3fdadea31f2075a2c10
Pawel <pawel.grzyb1985@gmail.com>
2019-03-29 22:18:25 +0100

    added configuration for authentication

--- src/main/java/com/pi4home/server/configurations/SpringSecurityConfig.java
+++ src/main/java/com/pi4home/server/configurations/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.pi4home.server.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter
 {
+
+    @Value("${security.user}")
+    private String user;
+    @Value("${security.password}")
+    private String password;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
@@ -24,8 +31,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter
             throws Exception
     {
         auth.inMemoryAuthentication()
-                .withUser(("${spring.security.user.name}"))
-                .password("${spring.security.user.password}")
+                .withUser(user)
+                .password("{noop}" + password)
                 .roles("USER");
     }
 }
