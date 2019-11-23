771a9bf81e23583d9b09c329e42faf7bc1510f92
Pawel <pawel.grzyb1985@gmail.com>
2019-11-23 22:12:47 +0100

    added logger, updated varsion to be consistent with other apps

--- src/main/java/com/pi4home/server/controller/YeelightController.java
+++ src/main/java/com/pi4home/server/controller/YeelightController.java
@@ -2,8 +2,13 @@ package com.pi4home.server.controller;
 
 import com.pi4home.server.model.Yeelight;
 import com.pi4home.server.services.YeelightService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -12,23 +17,13 @@ public class YeelightController
 {
     @Autowired
     YeelightService yeeslightService;
+    private static final Logger logger = LoggerFactory.getLogger(YeelightController.class);
 
-    @RequestMapping("/yeelight/{name}")
-    public List<Yeelight> light(@PathVariable String name)
-    {
-        return yeeslightService.switchYeelight(name);
-    }
-
-    @RequestMapping("/yeelightStatus")
-    public List<Yeelight> lightStatus()
-    {
-        return yeeslightService.getYeelightStatus();
-    }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/yeelight/")
-    public List<Yeelight> light(@RequestBody Yeelight yeelight)
+    public List<Yeelight> yeelight(@RequestBody Yeelight yeelight)
     {
-        System.out.println("RQ: " + yeelight.getName() + " turned on: " + yeelight.isTurnedOn());
+        logger.info("RQ: " + yeelight.getName() + " turned on: " + yeelight.isTurnedOn());
         yeeslightService.updateYeelightState(yeelight);
         return yeeslightService.getYeelightStatus();
     }
