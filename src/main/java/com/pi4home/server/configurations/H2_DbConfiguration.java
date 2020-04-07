77a7dff67100edda83e2cca3c151a6176f900bff
Pawel <pawel.grzyb1985@gmail.com>
2020-04-07 13:56:49 +0200

    commented dbConfiguration for H2 DB

--- src/main/java/com/pi4home/server/configurations/H2_DbConfiguration.java
+++ src/main/java/com/pi4home/server/configurations/H2_DbConfiguration.java
@@ -1,7 +1,7 @@
 package com.pi4home.server.configurations;
 
-import org.h2.tools.Server;
-import org.springframework.context.annotation.Bean;
+//import org.h2.tools.Server;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.sql.SQLException;
@@ -9,10 +9,10 @@ import java.sql.SQLException;
 @Configuration
 public class H2_DbConfiguration
 {
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server inMemoryH2DatabaseaServer() throws SQLException
-    {
-        return Server.createTcpServer(
-                "-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
-    }
+//    @Bean(initMethod = "start", destroyMethod = "stop")
+//    public Server inMemoryH2DatabaseaServer() throws SQLException
+//    {
+//        return Server.createTcpServer(
+//                "-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+//    }
 }
